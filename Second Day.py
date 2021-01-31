03168d86c8ae7f6c0f664914b901aac1c432fdac
mcphailjosh <78179600+mcphailjosh@users.noreply.github.com>
2021-02-01 09:22:29 +1300

    Create Second Day.py

---
+++ Second Day.py
@@ -0,0 +1,9 @@
+
+# Here a varible called name is storing the text "Josh Mcphail"
+name = "Josh Mcphail"
+
+print("Hello", name)
+
+# Here's a varible storing an interger
+number1 = 13
+print (number1 % 2)
