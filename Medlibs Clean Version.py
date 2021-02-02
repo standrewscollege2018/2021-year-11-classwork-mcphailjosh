e014625ba6d99eab05f38f709b08d61f300badc5
mcphailjosh <78179600+mcphailjosh@users.noreply.github.com>
2021-02-02 14:25:24 +1300

    Create Medlibs Clean Version.py

---
+++ Medlibs Clean Version.py
@@ -0,0 +1,8 @@
+print ("Hello, welcome to Madlibs")
+name = input ("What is your name")
+illness = input ("Enter an illness")
+subject = input ("Enter a subject")
+part = input ("Enter a body part")
+number = input("Enter a number higher than 5")
+
+print(" My son {} is very sore from his recent run in with {} so he will not be able to attend {} due to his {} and he is {} years old ".format(name, illness, subject, part, number))
