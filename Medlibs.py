dd8810c64f23460806d489d85c22870bbb8383f1
mcphailjosh <78179600+mcphailjosh@users.noreply.github.com>
2021-02-02 14:25:19 +1300

    Create Medlibs.py

---
+++ Medlibs.py
@@ -0,0 +1,11 @@
+# This program is a Madlib, getting the user to enter details
+# Then it prints out the story
+
+print ("Hello, welcome to Madlibs")
+name = input ("What is your name")
+illness = input ("Enter an illness")
+subject = input ("Enter a subject")
+part = input ("Enter a body part")
+
+# Print the story
+print("My son", name, "is stuck at home due to", illness, "he will not be at", subject,"becuase of his", part)
